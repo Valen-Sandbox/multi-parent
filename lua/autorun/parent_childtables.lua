23dc12f76200b476d2bb1f861d3cf1034c432c37
thecraftianman <64441307+thecraftianman@users.noreply.github.com>
2024-07-15 13:33:30 -0400

    Remove trailing whitespace

--- lua/autorun/parent_childtables.lua
+++ lua/autorun/parent_childtables.lua
@@ -10,26 +10,26 @@ local meta = FindMetaTable( "Entity" )
 if not meta.SetParentEngine then meta.SetParentEngine = meta.SetParent end
 
 function meta:SetParent( parent, attachment )
-	
+
 	local oldparent = self:GetParent()
 	self:SetParentEngine( parent, attachment )
-	
+
 	-- If we're unparenting or changing parent, remove the ent from the previous parent's childtable
 	if IsValid( oldparent ) and oldparent ~= parent and oldparent._children then
 		oldparent._children[ self ] = nil
 	end
-	
+
 	-- If we're parenting to a new/different ent, insert the ent into the parent's childtable
 	if IsValid( parent ) then
 		parent._children = parent._children or {}
 		parent._children[ self ] = self
 		self:CallOnRemove( "UnparentOnRemove", function( ent ) ent:SetParent( nil ) end )
 	end
-	
+
 end
 
 function meta:GetChildren()
-	
+
 	return self._children or {}
-	
+
 end
